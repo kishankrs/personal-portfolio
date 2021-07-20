5159cb083f93a96a0dd619159d0c4489dbf8802f
kishankrs <kishankrs@outlook.com>
2021-07-20 22:10:12 +0530

    initial setup and Nav bar

---
+++ personal-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import LandingSection from "./components/LandingSection/LandingSection";
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <LandingSection />
+    </main>
+  );
+};
+
+export default App;
---
+++ personal-portfolio/src/components/LandingSection/LandingSection.jsx
@@ -0,0 +1,42 @@
+import "./LandingSection.css";
+
+const LandingSection = () => {
+    return <section>
+        <header>
+            <nav>
+                <ul>
+                    <li> 
+                        <a data-Scroll href= "#landing">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a data-Scroll href= "#about">
+                        About
+                        </a>
+                    </li>
+                    <li>
+                        <a data-Scroll href= "#skills">
+                        Skills
+                        </a>
+                    </li>
+                    <li>
+                        <a data-Scroll href= "#projects">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:kishankrs21@gmail.com">Contact Me</a>
+                            
+                       
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+        <main></main>
+        </section>;
+    
+};
+
+export default LandingSection
