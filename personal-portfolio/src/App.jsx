0402d349d1c6abe71301e28f649516312a8f6594
kishankrs <kishankrs@outlook.com>
2022-01-14 15:36:12 +0530

    SkillSection added

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -3,12 +3,14 @@ import ProjectSection from "./components/ProjectSection/ProjectSection";
 import AboutSection from "./components/AboutSection/AboutSection";
 
 import "./App.css";
+import SkillSection from "./components/SkillSection/SkillSection";
 
 const App = () => {
   return (
     <main>
       <LandingSection />
       <AboutSection />
+      <SkillSection />
       <ProjectSection />
     </main>
   );
---
+++ personal-portfolio/src/components/SkillSection/SkillSection.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+
+import "./SkillSection.css"; //Stylesheet
+
+import { SKILLS } from "../../content/data"; //Data
+
+const SkillsSection = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to("#skills-heading", {
+      scrollTrigger: "#skills-heading",
+      opacity: 1,
+      duration: 2,
+      delay: 3.5,
+    })
+      .from("#skills-heading", {
+        x: -100,
+        duration: 2,
+        delay: -2,
+      })
+      .to(".skill-cont", {
+        scrollTrigger: ".skill-cont",
+        opacity: 1,
+        duration: 1.5,
+        delay: -1,
+      })
+      .from(".skill-cont", {
+        y: 100,
+        duration: 1.5,
+        delay: -1.5,
+      })
+      .from(".bar", {
+        height: 0,
+      });
+  }, []);
+
+  return (
+    <section className="skills" id="skills">
+      <header>
+        <h2 className="heading" id="skills-heading">
+          My Skills and Knowledge Set
+        </h2>
+      </header>
+      <main>
+        <article className="skill-cont">
+          {SKILLS.map(({ name, skill }, i) => (
+            <div className="skill" key={i}>
+              <div className="progress-bar">
+                <div className="bar" style={{ height: `${skill}%` }} />
+              </div>
+              <span>{name}</span>
+            </div>
+          ))}
+        </article>
+      </main>
+    </section>
+  );
+};
+
+export default SkillsSection;
